cb0142f63a93db825d06d1387589652e9f0d50a2
Mohamed Houssas <mhoussas26@gmail.com>
2025-03-05 21:46:34 +0000

    test

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mhoussas <mhoussas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 13:26:31 by mhoussas          #+#    #+#             */
-/*   Updated: 2025/02/26 14:30:31 by mhoussas         ###   ########.fr       */
+/*   Updated: 2025/03/03 14:39:53 by mhoussas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,9 @@
 
 int	n = 0;
 
-size_t	v_max = 10000;
+size_t	v_max = 100;
 
-void* ft_aid(void* arg) {
+void	*ft_aid(void* arg) {
 
 	int i = 0;
 	while (i++ < v_max)
@@ -30,8 +30,14 @@ void* ft_aid(void* arg) {
 
 int main() {
 	pthread_t thread_id;
+	pthread_t thread_id2;
 
-	if (pthread_create(&thread_id, NULL, ft_aid, NULL) != 0)
+	if (pthread_create(&thread_id, NULL, ft_aid, NULL))
+	{
+		perror("error to create Thread");
+		return 1;
+	}
+	if (pthread_create(&thread_id2, NULL, ft_aid, NULL))
 	{
 		perror("error to create Thread");
 		return 1;
@@ -40,6 +46,7 @@ int main() {
 	while (i++ < v_max)
 		n++;
 	pthread_join(thread_id, NULL);
+	pthread_join(thread_id2, NULL);
 	printf("%d\n", n);
 	printf("the main Thead end\n");
 	return 0;
